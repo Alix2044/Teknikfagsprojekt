4f47374c5e2e9968a724b1e713e3ca59808b96f6
EmmiPigen <73609450+EmmiPigen@users.noreply.github.com>
2023-04-19 14:56:37 +0200

    Formelsamling

---
+++ Formelsamling/Sider/procent.js
@@ -0,0 +1,10 @@
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    
+}
+
+function draw(){
+   // background(220)
+   rectMode(CENTER)
+   rect(width / 2, height / 2, width/)
+}
--- Formelsamling/sketch.js
+++ Formelsamling/sketch.js
@@ -2,35 +2,39 @@ let boksWidth;
 let boksActualWidth;
 let boksHeight;
 let boksY;
-//let knapværdi
+let plus;
+let gange;
+let divi;
+let brøk;
+let deci;
+let procent;
+let ligning;
+let hierarki
 
-
-let textFelter = ['Plus og Minus', 'Gange', 'Division', 'Brøker', 'Decimaltal', 'Procent', 'Ligninger','Regnehieraki'];
 function setup() {
   createCanvas(windowWidth - 10, windowHeight - 20);
-  
-  //let txt = createDiv("Here is some text and <a href='http://i.imgur.com/WXaUlrK.gif'>this is an HTML link</a>!"); 
-  plus = createButton("<a href='../Formelsamling/Sider/plus_minus.html'>Plus og Minus</a>");
-  gange = createButton("<a href='/Formelsamling/Sider/gange.html'>Gange</a>");
-  divi = createButton("<a href='/Formelsamling/Sider/divi.html'>Division</a>");
-  brøk = createButton("<a href='/Formelsamling/Sider/brøk.html'>Brøker</a>");
-  deci = createButton("<a href='/Formelsamling/Sider/deci.html'>Decimaltal</a>");
-  procent = createButton("<a href='/Formelsamling/Sider/procent.html'>Procent</a>");
-  ligning = createButton("<a href='/Formelsamling/Sider/ligning.html'>Ligninger</a>");
-  hiereaki = createButton("<a href='/Formelsamling/Sider/hierarki.html'>Hierarki</a>");
+  plus = createDiv("<a href='../Formelsamling/Sider/plus_minus.html'>Plus og Minus</a>");
+  gange = createDiv("<a href='../Formelsamling/Sider/gange.html'>Gange</a>");
+  divi = createDiv("<a href='../Formelsamling/Sider/divi.html'>Division</a>");
+  brøk = createDiv("<a href='../Formelsamling/Sider/brøk.html'>Brøker</a>");
+  deci = createDiv("<a href='../Formelsamling/Sider/deci.html'>Decimaltal</a>");
+  procent = createDiv("<a href='../Formelsamling/Sider/procent.html'>Procent</a>");
+  ligning = createDiv("<a href='../Formelsamling/Sider/ligning.html'>Ligninger</a>");
+  hierarki = createDiv("<a href='../Formelsamling/Sider/hierarki.html'>Hierarki</a>");
 }
 
 function draw() {
   //background(220)
   bokse();
   knapper();
+
 }
 
 function bokse(){
-  let boksWidth = (width / 4);
-  let boksActualWidth = boksWidth - width / 18
-  let boksHeight = (height / 4);
-  let boksY = height / 4 + boksHeight / 1.5
+  boksWidth = (width / 4);
+  boksActualWidth = boksWidth - width / 18
+  boksHeight = (height / 4);
+  boksY = height / 4 + boksHeight / 1.5
   let cornerR = 20;
   
   
@@ -51,18 +55,19 @@ function bokse(){
   }
 
   textAlign(CENTER, CENTER);
-  textSize(boksHeight / 5)
+  textSize(boksHeight / 3)
   noStroke();
   fill(0)
-  for(let i = 0; i<=3; i++){
+  text('Formelsamling', width / 2, (boksY - (boksHeight / 2)) / 2)
+
+ /* for(let i = 0; i<=3; i++){
   text(textFelter[i], boksWidth / 2 + (boksWidth * i), boksY);
   }
   for(let i = 0; i<=3; i++){
     text(textFelter[i + 4], boksWidth / 2 + (boksWidth * i), boksY + boksHeight * 1.5);
     }
-  textSize(boksHeight / 4)
-  text('Formelsamling', width / 2, (boksY - (boksHeight / 2)) / 2)
-
+ 
+  
 
   /*topBoksYmin = boksY - boksHeight / 2;
   topBoksYmax = topBoksYmin + boksHeight
@@ -89,5 +94,60 @@ function bokse(){
 }
 
 function knapper(){
+  fontsize = boksHeight / 4;
+  plus.style('font-size', fontsize + 'px')
+  plus.style('text-align: center');
+  plus.style('vertical-align: center');
+
+  plus.size(boksActualWidth, boksHeight);
+  plus.position(boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25, boksY - boksHeight / 10);
   
+  gange.style('font-size', fontsize + 'px')
+  gange.style('text-align: center');
+  gange.style('vertical-align: center');
+
+  gange.size(boksActualWidth, boksHeight);
+  gange.position((boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25) + boksWidth, boksY - boksHeight / 10);
+  
+  divi.style('font-size', fontsize + 'px')
+  divi.style('text-align: center');
+  divi.style('vertical-align: center');
+
+  divi.size(boksActualWidth, boksHeight);
+  divi.position((boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25) + boksWidth * 2, boksY - boksHeight / 10);
+  
+  brøk.style('font-size', fontsize + 'px')
+  brøk.style('text-align: center');
+  brøk.style('vertical-align: center');
+
+  brøk.size(boksActualWidth, boksHeight);
+  brøk.position((boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25) + boksWidth * 3, boksY - boksHeight / 10);
+ 
+  deci.style('font-size', fontsize + 'px')
+  deci.style('text-align: center');
+  deci.style('vertical-align: center');
+
+  deci.size(boksActualWidth, boksHeight);
+  deci.position(boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25, boksY - boksHeight / 10 + boksHeight * 1.5);
+
+  procent.style('font-size', fontsize + 'px')
+  procent.style('text-align: center');
+  procent.style('vertical-align: center');
+
+  procent.size(boksActualWidth, boksHeight);
+  procent.position((boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25) + boksWidth * 1, boksY - boksHeight / 10 + boksHeight * 1.5);
+
+  ligning.style('font-size', fontsize + 'px')
+  ligning.style('text-align: center');
+  ligning.style('vertical-align: center');
+
+  ligning.size(boksActualWidth, boksHeight);
+  ligning.position((boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25) + boksWidth * 2, boksY - boksHeight / 10 + boksHeight * 1.5);
+
+  hierarki.style('font-size', fontsize + 'px')
+  hierarki.style('text-align: center');
+  hierarki.style('vertical-align: center');
+
+  hierarki.size(boksActualWidth, boksHeight);
+  hierarki.position((boksWidth / 2 - boksActualWidth / 2 + boksActualWidth / 25) + boksWidth * 3, boksY - boksHeight / 10 + boksHeight * 1.5);
 }
